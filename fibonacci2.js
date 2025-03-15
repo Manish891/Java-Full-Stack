cd5337629e6a0cccbfb40bae2be5e52132e40394
Manish891 <siddhanthammanish5@gmail.com>
2025-03-16 00:39:13 +0530

    Delete fibonacci2.js

--- fibonacci2.js
+++
@@ -1,9 +0,0 @@
-function fibseries_02(num) {
-    let a=0,b=1;
-    let series=[a,b];
-    for (let i=2;i<num;i++){
-        series[i]=series[i-1]+series[i-2];
-    }
-    return series;
-}
-console.log(fibseries_02(10));
