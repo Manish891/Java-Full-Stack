e6d4181ad90a8a7516d7b6efa18e598a24f293d6
Manish891 <siddhanthammanish5@gmail.com>
2025-03-16 00:35:35 +0530

    Delete dayofweek.js

--- dayofweek.js
+++
@@ -1,9 +0,0 @@
-let DAYS = "SUNMONTUEWEDTHUFRISAT";
-let day = "TUESDAY";
-day = day.substring(0,3).toUpperCase();
-console.log(DAYS.indexOf(day)/3 + 1);
-
-let MONTHS = "JANFEBMARAPRMAYJUNJULAUGSEPOCTNOVDEC";
-let month = "JULY";
-month = month.substring(0,3).toUpperCase();
-console.log(MONTHS.indexOf(month)/3 + 1);
