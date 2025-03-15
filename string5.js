214dda7225aed807b14c3bdebc52276cf00a2ad7
Manish891 <siddhanthammanish5@gmail.com>
2025-03-16 00:29:21 +0530

    Delete string5.js

--- string5.js
+++
@@ -1,8 +0,0 @@
-const str = 'Siddhantham Manish';
-let revStr = '';
-for (let i = str.length - 1;i>=0;i--){
-    revStr +=str[i]
-}
-console.log(revStr);
-
-console.log(str.split('').reverse().join(''));
