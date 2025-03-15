fa4f460a39d927a11f6ddc4b46641e2da8ab82d0
Manish891 <siddhanthammanish5@gmail.com>
2025-03-16 00:24:32 +0530

    Delete 9leapyear.js

--- 9leapyear.js
+++
@@ -1,9 +0,0 @@
-let year = process.argv[2]
-console.log("Enter The Year Is : ",year)
-if (year % 4 == 0){
-    console.log("Leap Year !")
-}
-else {
-    console.log("It Is Not A Leap Year !");
-    
-}
