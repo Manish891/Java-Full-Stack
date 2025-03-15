2a93d42fce6f7766559a6c31f72f46cc7f3f8fd7
Manish891 <siddhanthammanish5@gmail.com>
2025-03-16 00:30:51 +0530

    Delete ascii.js

--- ascii.js
+++
@@ -1,8 +0,0 @@
-function asciiSum(str) {
-    let sum = 0; 
-    for (let i = 0; i < str.length; i++) {
-        sum = sum + str.charCodeAt(i); 
-    }
-    return sum;
-}
-console.log(asciiSum("Hello people welcome back to the show")); 
