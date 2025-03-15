84cdf94e718a9cb3cfc9c5a6c121de32088e9530
Manish891 <siddhanthammanish5@gmail.com>
2025-03-16 00:25:08 +0530

    Delete Concat.js

--- Concat.js
+++
@@ -1,7 +0,0 @@
-const Str1 = 'Siddhantham';
-const Str2 = 'Manish';
-
-console.log(Str2.concat(' ',Str1));
-console.log(Str2.concat('.',Str1));
-console.log(Str2.concat(',',Str2));
-console.log(Str1.concat('/',Str2));
